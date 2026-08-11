66c2853c0d63d5a3315ae86d57445a255bfd3308
Adrian-Nicușor Necula <adriannicusorn@yahoo.com>
2026-08-11 21:09:39 +0300

    Add files via upload

---
+++ src/temp_converter.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.08.2026 17:52:56
+// Design Name: 
+// Module Name: temp_converter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module temp_converter (
+    input  logic [15:0] temperature_raw,
+    output logic [7:0] temp_celsius,
+    output logic [3:0] temp_fraction,
+    output logic negative
+);
+
+integer temp_value;
+
+always @(*) begin
+    temp_value = ($signed(temperature_raw[15:3]) * 10) / 16;
+    if (temp_value < 0) begin
+        negative = 1'b1;
+        temp_value = -temp_value;
+    end
+    else begin
+        negative = 1'b0;
+    end
+    temp_celsius = temp_value / 10;
+    temp_fraction = temp_value % 10;
+end
+
+endmodule
---
+++ src/temp_to_digits.sv
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.08.2026 18:46:21
+// Design Name: 
+// Module Name: temp_to_digits
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module temp_to_digits(
+    input logic [7:0] temp_celsius,
+    input logic [3:0] temp_fraction,
+    output logic [3:0] digit0,
+    output logic [3:0] digit1,
+    output logic [3:0] digit2,
+    output logic [3:0] digit3
+);
+
+always @(*) begin
+    if (temp_celsius >= 10)
+        digit3 = (temp_celsius / 10) % 10;
+    else
+        digit3 = 4'hF;     // blank
+    
+    digit2 = temp_celsius % 10;
+    digit1 = temp_fraction;
+    digit0 = 4'hA;         // grad
+end
+
+endmodule
