4231b3f4c738675da701dbf0d74b29ed6ffd1a21
Adrian-Nicușor Necula <adriannicusorn@yahoo.com>
2026-08-18 03:23:29 +0300

    Update decodor_anod.sv

--- src/display/decodor_anod.sv
+++ src/display/decodor_anod.sv
@@ -20,26 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module decodor_anod(
+    input rst,
     input [2:0] sel,
     output reg [7:0] an
 );
 
 always @(*) begin
-
-    case (sel)
-        3'd0: an = 8'b11111110;
-        3'd1: an = 8'b11111101;
-        3'd2: an = 8'b11111011;
-        3'd3: an = 8'b11110111;
-        3'd4: an = 8'b11101111;
-        3'd5: an = 8'b11011111;
-        3'd6: an = 8'b10111111;
-        3'd7: an = 8'b01111111;
-
-        default: an = 8'b11111111;
-
-    endcase
-
+    if (rst) begin
+        an = 8'b11111111;
+    end
+    else begin
+        case (sel)
+            3'd0: an = 8'b11111110;
+            3'd1: an = 8'b11111101;
+            3'd2: an = 8'b11111011;
+            3'd3: an = 8'b11110111;
+            3'd4: an = 8'b11101111;
+            3'd5: an = 8'b11011111;
+            3'd6: an = 8'b10111111;
+            3'd7: an = 8'b01111111;
+            
+            default: an = 8'b11111111;
+        endcase
+    end
 end
 
 endmodule
